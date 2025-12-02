7307340f988b40a6edbb8c0e7a40d85d1559b42f
yuukikuno <kunoyuuki@proton.me>
2025-12-02 09:49:37 +0100

    Organize code

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,65 +1,6 @@
-use std::str::FromStr;
+use advent_of_code::dial::{Dial, Rotation};
 
 advent_of_code::solution!(1);
-
-struct Dial {
-    position: i32,
-    point_password: u64,
-    click_password: u64,
-}
-
-enum Direction {
-    Left,
-    Right,
-}
-
-impl TryFrom<char> for Direction {
-    type Error = ();
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            'L' => Ok(Self::Left),
-            'R' => Ok(Self::Right),
-            _ => Err(()),
-        }
-    }
-}
-
-struct Rotation {
-    direction: Direction,
-    distance: u64,
-}
-
-impl FromStr for Rotation {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Rotation {
-            direction: Direction::try_from(s.chars().next().unwrap())?,
-            distance: s[1..].parse().unwrap(),
-        })
-    }
-}
-
-impl Dial {
-    pub fn new(position: i32) -> Self {
-        Dial {
-            position,
-            point_password: 0,
-            click_password: 0,
-        }
-    }
-
-    pub fn turn(&mut self, rotation: Rotation) {
-        let delta = match rotation.direction {
-            Direction::Left => -(rotation.distance as i32),
-            Direction::Right => rotation.distance as i32,
-        };
-        self.position = (self.position + delta).rem_euclid(100);
-        if self.position == 0 {
-            self.point_password += 1;
-        }
-    }
-}
-
 pub fn part_one(input: &str) -> Option<u64> {
     let mut dial = Dial::new(50);
     for line in input.lines() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod template;
-
+pub mod dial;
 // Use this file to add helper functions and additional modules.
