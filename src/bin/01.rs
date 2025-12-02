677405efd8ffb53235a3cbefebe9a858492fd448
yuukikuno <kunoyuuki@proton.me>
2025-12-02 09:28:04 +0100

    Clean up Day 1 Part 1 and attempt part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,75 @@
+use std::str::FromStr;
+
 advent_of_code::solution!(1);
 
+struct Dial {
+    position: i32,
+    point_password: u64,
+    click_password: u64,
+}
+
+enum Direction {
+    Left,
+    Right,
+}
+
+impl TryFrom<char> for Direction {
+    type Error = ();
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' => Ok(Self::Left),
+            'R' => Ok(Self::Right),
+            _ => Err(()),
+        }
+    }
+}
+
+struct Rotation {
+    direction: Direction,
+    distance: u64,
+}
+
+impl FromStr for Rotation {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Rotation {
+            direction: Direction::try_from(s.chars().next().unwrap())?,
+            distance: s[1..].parse().unwrap(),
+        })
+    }
+}
+
+impl Dial {
+    pub fn new(position: i32) -> Self {
+        Dial {
+            position,
+            point_password: 0,
+            click_password: 0,
+        }
+    }
+
+    pub fn turn(&mut self, rotation: Rotation) {
+        let delta = match rotation.direction {
+            Direction::Left => -(rotation.distance as i32),
+            Direction::Right => rotation.distance as i32,
+        };
+        self.position = (self.position + delta).rem_euclid(100);
+        if self.position == 0 {
+            self.point_password += 1;
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
+    let mut dial = Dial::new(50);
+    for line in input.lines() {
+        let rotation: Rotation = line.parse().unwrap();
+        dial.turn(rotation);
+    }
+    Some(dial.point_password)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
     let mut count = 0;
     let mut pos = 50;
     for line in input.lines() {
@@ -13,16 +82,11 @@ pub fn part_one(input: &str) -> Option<u64> {
             _ => panic!(),
         };
         pos += distance * sign;
-        pos = pos %100;
-        if pos == 0 {
-            count += 1;
-        }
+        //todo count when pos = 0 but dont count when going 0 -> something else (probs -1?)
+        count += pos.div_euclid(100).unsigned_abs();
+        pos = pos.rem_euclid(100);
     }
-    Some(count)
-}
-
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(count as u64)
 }
 
 #[cfg(test)]
@@ -32,12 +96,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result.unwrap(), 3);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
