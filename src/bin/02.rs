4b2484e737e36d082225a5087b5c28999597e008
yuukikuno <kunoyuuki@proton.me>
2025-12-03 08:55:36 +0100

    Day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 advent_of_code::solution!(2);
 
 pub fn digits_repeat_twice(id: u64) -> bool {
@@ -24,8 +22,27 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(result)
 }
 
+pub fn digits_repeat(id: u64) -> bool {
+    let digits = id.to_string();
+    let len = digits.len();
+    (1..=len / 2)
+        .filter(|&sub_len| len.is_multiple_of(sub_len))
+        .any(|sub_len| {
+            let mut sequences = digits.as_bytes().chunks(sub_len);
+            let first = sequences.next().unwrap();
+            sequences.all(|sequence| sequence == first)
+        })
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut result: u64 = 0;
+    for id_range in input.split(',') {
+        let (start, end) = id_range.split_once('-').unwrap();
+        let (start, end): (u64, u64) = (start.parse().unwrap(), end.parse().unwrap());
+
+        result += (start..=end).filter(|&id| digits_repeat(id)).sum::<u64>();
+    }
+    Some(result)
 }
 
 #[cfg(test)]
@@ -41,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
