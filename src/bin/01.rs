573a4af8abbcb758fa2a292a7f92ffd995d4cf9c
yuukikuno <kunoyuuki@proton.me>
2025-12-04 10:34:13 +0100

    Go back to one file a day

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,74 @@
-use advent_of_code::dial::{Dial, Rotation};
+use std::str::FromStr;
 
 advent_of_code::solution!(1);
+
+struct Dial {
+    position: i32,
+    pub point_password: u64,
+    pub click_password: u64,
+}
+
+impl Dial {
+    pub fn new(position: i32) -> Self {
+        Dial {
+            position,
+            point_password: 0,
+            click_password: 0,
+        }
+    }
+
+    pub fn turn(&mut self, rotation: Rotation) {
+        let initial = self.position;
+        let delta = match rotation.direction {
+            Direction::Left => -(rotation.distance as i32),
+            Direction::Right => rotation.distance as i32,
+        };
+        self.click_password += get_clicks(initial, initial + delta);
+        self.position = (initial + delta).rem_euclid(100);
+        if self.position == 0 {
+            self.point_password += 1;
+        }
+    }
+}
+
+fn get_clicks(start: i32, end: i32) -> u64 {
+    (match (start, end) {
+        (0, _) => end.abs().div_euclid(100),
+        (_, ..=0) => end.abs().div_euclid(100) + 1,
+        (_, _) => end.div_euclid(100),
+    }) as u64
+}
+enum Direction {
+    Left,
+    Right,
+}
+
+impl TryFrom<char> for Direction {
+    type Error = ();
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' => Ok(Self::Left),
+            'R' => Ok(Self::Right),
+            _ => Err(()),
+        }
+    }
+}
+
+struct Rotation {
+    direction: Direction,
+    distance: u64,
+}
+
+impl FromStr for Rotation {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Rotation {
+            direction: Direction::try_from(s.chars().next().unwrap())?,
+            distance: s[1..].parse().unwrap(),
+        })
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let mut dial = Dial::new(50);
     for line in input.lines() {
