57266a4f3f50f4511be6e86e02dd80fbda5a2e79
yuukikuno <kunoyuuki@proton.me>
2025-12-04 10:12:38 +0100

    Day 4 part 1 ugly

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,34 @@
+use std::ops::RangeInclusive;
+
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut result = 0;
+    let grid = &input.lines().collect::<Vec<&str>>();
+    for (y, line) in grid.iter().enumerate() {
+        for (x, &char) in line.as_bytes().iter().enumerate() {
+            if char == b'.' {
+                continue;
+            }
+            let adjacent_count = grid[neighbor_range(grid, y)]
+                .iter()
+                .flat_map(|neighbor_row| {
+                    let bytes = neighbor_row.as_bytes();
+                    bytes[neighbor_range(bytes, x)].iter()
+                })
+                .filter(|&&adjacent_cell| adjacent_cell == b'@')
+                .count()
+                - 1; //self
+            if adjacent_count < 4 {
+                result += 1
+            }
+        }
+    }
+    Some(result)
+}
+
+fn neighbor_range<T>(slice: &[T], index: usize) -> RangeInclusive<usize> {
+    index.saturating_sub(1)..=(index + 1).min(slice.len() - 1)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +42,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]
