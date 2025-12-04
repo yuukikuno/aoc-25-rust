424fce5907152ca7e3202925387b0a21772570c0
yuukikuno <kunoyuuki@proton.me>
2025-12-04 07:56:53 +0100

    Day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,32 +1,58 @@
+use std::ops::Deref;
+
 advent_of_code::solution!(3);
 
+fn parse_digits(s: &str) -> Vec<u8>{
+    s.chars()
+        .map(|char| char.to_digit(10).unwrap() as u8)
+        .collect()
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    //parse batteries: 1 line for each
     Some(
         input
             .lines()
             .map(|batteries| {
-                let batteries: Vec<u32> = batteries
-                    .chars()
-                    .map(|char| char.to_digit(10).unwrap())
-                    .collect();
-                let (first_battery_pos, first_battery) = batteries
-                    .clone()
-                    .into_iter()
-                    .enumerate()
-                    .rev()
-                    .skip(1)
-                    .max_by_key(|&(_i, x)| x)
-                    .unwrap();
-                let second_battery = batteries[first_battery_pos + 1..].iter().max().unwrap();
-                (first_battery * 10 + second_battery) as u64
+                let batteries: Vec<u8> = parse_digits(batteries);
+
+                get_joltage(batteries, 2)
             })
             .sum::<u64>(),
     )
 }
 
+fn get_joltage(batteries: Vec<u8>, count: usize) -> u64 {
+    let (_, joltage) =
+        (0..count)
+            .rev()
+            .fold((batteries, 0), |(batteries, joltage), n| {
+                let (pos, battery) = find_largest(n, batteries.clone());
+                (batteries[pos + 1..].to_owned(), joltage + battery as u64 * 10_u64.pow(n as u32))
+            });
+    joltage
+}
+
+fn find_largest(n: usize, batteries: Vec<u8>) -> (usize, u8) {
+    let (battery_pos, battery) = batteries
+        .into_iter()
+        .enumerate()
+        .rev()
+        .skip(n)
+        .max_by_key(|&(_i, x)| x)
+        .unwrap();
+    (battery_pos, battery)
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|batteries| {
+                let batteries: Vec<u8> = parse_digits(batteries);
+                get_joltage(batteries, 12)
+            })
+            .sum::<u64>(),
+    )
 }
 
 #[cfg(test)]
@@ -42,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
