961bd42e26eb9f76605d0191d073a52c88e54fdc
yuukikuno <kunoyuuki@proton.me>
2025-12-06 16:12:44 +0100

    day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,11 +1,50 @@
+use std::ops::RangeInclusive;
+
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let (id_ranges, ids) = input.split_once("\n\n").unwrap();
+    let id_ranges = id_ranges.split('\n').map(|id_range| {
+        let (start, end) = id_range.split_once('-').unwrap();
+        start.parse::<u64>().unwrap()..=end.parse::<u64>().unwrap()
+    });
+    Some(
+        ids.split('\n')
+            .map(|id| id.parse::<u64>().unwrap())
+            .filter(|id| id_ranges.clone().any(|id_range| id_range.contains(id)))
+            .count() as u64,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (id_ranges, _ids) = input.split_once("\n\n").unwrap();
+    let mut id_ranges: Vec<(u64, u64)> = id_ranges
+        .split('\n')
+        .map(|id_range| {
+            let (start, end) = id_range.split_once('-').unwrap();
+            (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap())
+        })
+        .collect();
+    id_ranges.sort_by(|(a, _), (b, _)| a.cmp(b));
+    let mut merged_ranges: Vec<(u64, u64)> = vec![];
+    for id_range in id_ranges {
+        if merged_ranges.is_empty() {
+            merged_ranges.push(id_range);
+            continue;
+        }
+        let last = merged_ranges.last_mut().unwrap();
+        if last.1 + 1 >= id_range.0 {
+            last.1 = last.1.max(id_range.1);
+        } else {
+            merged_ranges.push(id_range);
+        }
+    }
+    Some(
+        merged_ranges
+            .into_iter()
+            .map(|id_range| (id_range.0..=id_range.1).count())
+            .sum::<usize>() as u64,
+    )
 }
 
 #[cfg(test)]
@@ -15,12 +54,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
