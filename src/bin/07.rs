99d8e60efaa7a8e8b63ef78b2d2928953be2f8de
yuukikuno <kunoyuuki@proton.me>
2025-12-07 23:32:35 +0100

    Day 7 part 1 faster

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,25 +4,39 @@ use std::path::Component::ParentDir;
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut lines = input.lines().map(|line| line.as_bytes()).step_by(2);;
-    let starting_pos = lines.next()?.iter().position(|&char| char == b'S').expect("missing start");
-    let mut beams_pos: Vec<usize> = [starting_pos].into();
+    let mut lines = input.lines().map(|line| line.as_bytes()).step_by(2);
+    let mut first = lines
+        .next()?
+        .iter();
+    let width = first.len();
+    let starting_pos = first
+        .position(|&char| char == b'S')
+        .expect("missing start");
+    let mut beams_pos: Vec<usize> = vec![starting_pos];
+    let mut next_beams_pos = Vec::new();
+
+    let mut bitmap = vec![false; width];
     let mut result = 0;
 
-    let mut next_beams_pos = Vec::new();
     for line in lines {
         next_beams_pos.clear();
-        for &x in &beams_pos{
-            if line[x] == b'^'{
+        bitmap.fill(false);
+
+        for &x in &beams_pos {
+            let targets = if line[x] == b'^' {
                 result += 1;
-                next_beams_pos.push(x-1);
-                next_beams_pos.push(x+1);
+                [x - 1, x + 1]
             } else {
-                next_beams_pos.push(x);
+                [x, x]
+            };
+            for &t in &targets {
+                if !bitmap[t] {
+                    bitmap[t] = true;
+                    next_beams_pos.push(t);
+                }
             }
         }
-        next_beams_pos.sort();
-        next_beams_pos.dedup();
+
         std::mem::swap(&mut beams_pos, &mut next_beams_pos);
     }
     Some(result)
